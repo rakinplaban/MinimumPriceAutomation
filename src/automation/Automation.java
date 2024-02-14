2ea6b7e9cdaf5736970f3e3fe72372be799e8656
rakinplaban <rakinshahriar54@gmail.com>
2024-02-14 19:26:44 +0600

    Erronious code.

---
+++ src/automation/Automation.java
@@ -0,0 +1,124 @@
+package automation;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+import java.util.HashMap;
+
+import java.util.List;
+
+import java.util.Map;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.junit.After;
+
+import org.junit.Before;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.Dimension;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+import org.openqa.selenium.Keys;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.openqa.selenium.interactions.Actions;
+
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Automation {
+
+	private WebDriver dr;
+
+	private Map<String, Object> vars;
+	JavascriptExecutor js;
+
+	@Before
+	public void setUp() {
+		dr = new FirefoxDriver();
+		js = (JavascriptExecutor) dr;
+		vars = new HashMap<String, Object>();
+	}
+
+	@After
+	public void tearDown() {
+	    dr.quit();
+	}
+
+	
+    @Test
+    public void cart() throws Throwable {
+
+		dr.get("https://www.makemytrip.com/flight/search?itinerary=DEL-BOM-14/02/2024&tripType=O&paxType=A-1_C-0_I-0&intl=false&cabinClass=E&ccde=IN&lang=eng&cmp=SEM|D|IF|G|Brand|IF_Brand_Exact-Ex_India|Brand_Exact_Ex_india|RSA|Regular|NewFunnel|645153719700");
+		dr.manage().window().maximize();
+		WebDriverWait wait = new WebDriverWait(dr, Duration.ofSeconds(5));
+			//get all the prices from the table
+
+		List<WebElement> price = dr.findElements(By.xpath("//*[@id='content']/div/div[5]/div[5]/div[2]/div/div[2]/div[2]/div[6]/p[1]/span[2]"));
+
+		System.out.println(price.size());
+
+		//put all the prices into array list and get the lowest prices
+
+		ArrayList<Integer> prices=new ArrayList<Integer>();
+
+		for(int i=0;i<price.size();i++){
+		//System.out.println(price.get(i).getText());
+			Integer priceInt = Integer.valueOf(price.get(i).getText().replace(",", ""));
+			prices.add(priceInt);		
+		}
+
+		Integer minPrice = Collections.min(prices);
+
+		System.out.println("Min Price is "+minPrice);
+
+
+		//compare all the prices with lowest price and click the corresponding book button
+
+		List<WebElement> allBookbtn = dr.findElements(By.xpath("//*[@id='content']/div/div[5]/div[5]/div[2]/div/div[2]/div[2]/div[7]/p/a"));
+
+			Thread.sleep(3000);
+
+			for(int i=0;i<price.size();i++){
+
+			Integer priceInt1 = Integer.valueOf(price.get(i).getText().replace(",", ""));
+
+			//String price1 = price.get(i).getText();
+
+			System.out.println(priceInt1);
+
+			if(priceInt1==minPrice){
+
+			allBookbtn.get(i).click();
+
+			break;
+
+			}
+
+		}
+
+	 }
+
+	
+
+}
+
+
